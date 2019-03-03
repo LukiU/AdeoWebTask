a4aff81e2f8d317737f790824275ea07cb64cc92
LukiU <l.pangonis@gmail.com>
2019-03-03 02:27:33 +0200

    Updates

---
+++ precache-manifest.35ed3c5db7cd83d08a0b900cac2923fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c082f98d7ce28e8bf0ef",
+    "url": "/AdeoWebTask/static/css/main.bffc04c4.chunk.css"
+  },
+  {
+    "revision": "c082f98d7ce28e8bf0ef",
+    "url": "/AdeoWebTask/static/js/main.c082f98d.chunk.js"
+  },
+  {
+    "revision": "3f092ce1b9994083ff0c",
+    "url": "/AdeoWebTask/static/js/runtime~main.3f092ce1.js"
+  },
+  {
+    "revision": "4ef306e09dea2c49ea29",
+    "url": "/AdeoWebTask/static/js/2.4ef306e0.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/AdeoWebTask/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7d868576326875397bbf6cf1118718ef",
+    "url": "/AdeoWebTask/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/AdeoWebTask/precache-manifest.35ed3c5db7cd83d08a0b900cac2923fd.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/AdeoWebTask/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
